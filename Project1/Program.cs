9d02ec719d0e0a930f5e3032234a381bd5598f30
Blaise Turkish <blaise.turkish@gmail.com>
2017-11-19 22:54:23 +0100

    Commit message

---
+++ Project1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Török Balázs");
+            Console.WriteLine(2 + 2);
+        }
+    }
+}
